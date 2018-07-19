facb6d06792090012d3e4c6e913ed25a1db655ee
Serhii Nykodiuk <paradox.ne@gmail.com>
2018-07-19 07:19:03 +0300

    Completed RecyclerView

---
+++ app/src/main/java/labs/nyko/criminalintent/CrimeActivity.java
@@ -0,0 +1,10 @@
+package labs.nyko.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
