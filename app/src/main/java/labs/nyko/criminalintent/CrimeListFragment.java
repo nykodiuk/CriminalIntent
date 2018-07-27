2bcf7f5ca85b0f509f8eebfe4fbd96737a6b375b
Serhii Nykodiuk <paradox.ne@gmail.com>
2018-07-27 04:07:49 +0300

    Finished chapter 10

--- app/src/main/java/labs/nyko/criminalintent/CrimeListFragment.java
+++ app/src/main/java/labs/nyko/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package labs.nyko.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -31,11 +32,21 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     private void updateUI(){
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if(mAdapter==null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -54,7 +65,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(),mCrime.getTitle() + " clicked", Toast.LENGTH_SHORT).show();
+            Intent intent = CrimeActivity.newIntent(getActivity(),mCrime.getID());
+            startActivity(intent);
         }
 
         public void bind(Crime crime){
